33d62f090e6047d6d261beec3b8f292d395b6798
RishiiJain <rishijain9739@gmail.com>
2023-08-17 14:16:36 +0530

    LOGIN Final

--- src/components/login/style.js
+++ src/components/login/style.js
@@ -4,12 +4,11 @@ const LogIn = styled.div`
   margin: 0;
   padding: 0;
   font-family: Arial, sans-serif;
-  background-color: #6D9DC5;
+  background-color: #D6EFFF;
 `;
 
 const Heading = styled.h2`
-  color: #FE654F;
-  font-size: 30px;
+  font-size: 3rem;
   text-align: center;
 `;
 
@@ -17,15 +16,11 @@ const SignInContainer = styled.div`
   background-color: #ffffff;
   width: 100%;
   max-width: 40rem;
-  padding: 5rem;
+  padding: 3rem;
   margin: auto;
-  /* margin-top: 10rem ; */
-  border-radius: 0.8rem;
+  border-radius: 1.5rem;
   width: fit-content;
-  position: center;
-  align-items: center;
-  gap: 1rem;
-  box-shadow: 0.5px 0.5px 0.8px rgba(0, 0, 0, 0.3);
+  box-shadow: 0.5rem 0.5rem 0.8rem rgba(0, 0, 0, 0.3);
 `;
 
 const SignInForm = styled.div`
@@ -35,36 +30,9 @@ const SignInForm = styled.div`
 
 const SignInInput = styled.input`
   margin: 10px 0;
-  padding: 10px;
   border: 1px solid #ccc;
   border-radius: 4px;
   font-size: 14px;
-`;
-
-const SignInButton = styled.button`
-  background-color: #007bff;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  padding: 10px;
-  font-size: 16px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`;
-
-const Button = styled.button`
-  display: block;
-  width: 100%;
-  padding: 10px;
-  background-color: #007bff;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
 `;
 
 const SignUpLink = styled.div`
@@ -85,8 +53,8 @@ export {
   SignInContainer,
   SignInForm,
   SignInInput,
-  SignInButton,
-  Button,
+  // SignInButton,
+  // Button,
   SignUpLink,
   SignUpAnchor,
 };
